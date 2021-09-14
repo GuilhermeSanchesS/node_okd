b9abada06ace0052b87eed0aaa1f00f53c5f2296
G8U17 <guilhermebr18@hotmail.com>
2021-09-14 12:49:43 -0300

    Create project

---
+++ index.js
@@ -0,0 +1,16 @@
+//Inclusão do módulo necessário
+const http = require("http");
+
+//define a porta para acesso à aplicação
+const PORT = process.env.PORT || 8000
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-type": "text/html",
+  });
+  res.end("<h1>Bem-vindo SDC</h1><p>OpenShift NodeJS App</p>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em localhost:${PORT}`);
+});
